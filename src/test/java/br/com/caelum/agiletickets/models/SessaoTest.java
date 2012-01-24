34b2b1be3d0d6c38dedcec45ea365c01ef55f949
barbara <bdsp.pe@gmail.com>
2012-01-24 11:39:24 -0200

    refatoracao da classe de teste para a sessao

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -15,21 +15,18 @@ public class SessaoTest {
 	
 	@Test
 	public void deveVender1ingressoSeHa2vagas() throws Exception {
-		
         sessao.setTotalIngressos(2);
         Assert.assertTrue(sessao.podeReservar(1));
 	}
 
 	@Test
 	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
-		
 		sessao.setTotalIngressos(2);
 		Assert.assertFalse(sessao.podeReservar(3));
 	}
 
 	@Test
 	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis() throws Exception {
-		
 		sessao.setTotalIngressos(5);
 		sessao.reserva(3);
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
